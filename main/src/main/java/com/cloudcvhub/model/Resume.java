bc1c28acf4124e0ee37718825753de283c7fcdf7
quoczai22 <kienquoctrinhfacebook@gmail.com>
2026-07-23 13:44:56 +0700

    tao_cac_model_cho_project_them_getset_constructor

---
+++ main/src/main/java/com/cloudcvhub/model/Resume.java
@@ -0,0 +1,4 @@
+package com.cloudcvhub.model;
+
+public class Resume {
+}
---
+++ main/src/main/java/com/cloudcvhub/utils/HibernateUtil.java
@@ -0,0 +1,28 @@
+package com.cloudcvhub.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private HibernateUtil(){}
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Exception e) {
+            System.err.println("Can not init SessionFactory: " + e.getMessage());
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if(sessionFactory != null){
+            sessionFactory.close();
+        }
+    }
+}
