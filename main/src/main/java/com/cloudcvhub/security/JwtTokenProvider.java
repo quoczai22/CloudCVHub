b8e0d7c75e1287008409fc1028af010475ff2de8
Trịnh Hữu Kiến Quốc <kienquoctrinhfacebook@gmail.com>
2026-07-25 02:01:50 +0700

    Update JwtTokenProvider.java

--- main/src/main/java/com/cloudcvhub/security/JwtTokenProvider.java
+++ main/src/main/java/com/cloudcvhub/security/JwtTokenProvider.java
@@ -5,7 +5,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import jakarta.annotation.PostConstruct; // Dùng để khởi tạo key sau khi đọc được chuỗi bí mật
+import jakarta.annotation.PostConstruct; 
 import java.nio.charset.StandardCharsets;
 
 import javax.crypto.SecretKey;
